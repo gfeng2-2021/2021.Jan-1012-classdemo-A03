76e87b80ba1530718d945f3b94de084316d61b81
gfeng2-2021 <77166492+gfeng2-2021@users.noreply.github.com>
2021-01-15 11:18:32 -0700

    first plan sample

---
+++ myFirstPlanSolution/myFirstPlan/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace myFirstPlan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get home from a bar plan.
+            Console.WriteLine("Decide to call a Uber");
+            Console.WriteLine("Open the Uber app.");
+            Console.WriteLine("Enter current address.");
+            Console.WriteLine("Enter desitination address");
+            Console.WriteLine("Submit order and wait for the driver to confirm.");
+            Console.WriteLine("Wait for driver to arrive after confirmation");
+            Console.WriteLine("Get onto the car and get home.");
+
+        }
+    }
+}
